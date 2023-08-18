0310f2f957737bfb61aa2a2bdc3b0e7b8df0ce47
Johanx22x <johanrodsa2210@gmail.com>
2023-08-18 22:45:40 +0000

    Update main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/Johanx22x/GoTune"
-	"github.com/Johanx22x/GoTune/internal/db/repositories"
+	"github.com/Johanx22x/GoTune/internal/api"
+	"github.com/Johanx22x/GoTune/internal/db"
+	"github.com/gorilla/mux"
 )
 
 func main() {
+    // Load config
     config := gotune.LoadConfig(true)
 
+    // Connect to database
     db, err := repositories.NewDatabase(config.DatabaseFile)
     if err != nil {
         panic(errors.New("could not connect to database"))
     }
     fmt.Println("Database connection established")
     defer db.Close()
+
+    // Create router
+    router := mux.NewRouter()
+
+    // Register routes
+    api.RegisterRoutes(router, db)
+
+    // Serve the API on designated port
+    fmt.Println("Serving API on port " + config.Port)
+    http.Handle("/", router)
+    http.ListenAndServe(":"+config.Port, nil)
 }
