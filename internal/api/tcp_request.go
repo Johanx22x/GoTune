86bbf78ab12fd9f52716532d66cc62e2313158ec
Johanx22x <johanrodsa2210@gmail.com>
2023-09-01 16:12:33 +0000

    Remove unnecessary features

--- internal/api/tcp_request.go
+++ internal/api/tcp_request.go
@@ -41,26 +41,12 @@ func processGetRequest(words []string, db *sql.DB) (string, error) {
         } else {
             return songsHandler.GetSong(words[2])
         }
-    case "users":
-        usersHandler := handlers.NewUserHandler(repositories.NewUserRepository(db))
-        if len(words) == 2 {
-            return usersHandler.GetUsers()
-        } else {
-            return usersHandler.GetUser(words[2])
-        }
     case "playlists":
         playlistsHandler := handlers.NewPlaylistHandler(repositories.NewPlaylistRepository(db))
         if len(words) == 2 { 
             return playlistsHandler.GetPlaylists()
         } else { 
             return playlistsHandler.GetPlaylist(words[2])
-        }
-    case "albums":
-        albumsHandler := handlers.NewAlbumHandler(repositories.NewAlbumRepository(db))
-        if len(words) == 2 { 
-            return albumsHandler.GetAlbums()
-        } else { 
-            return albumsHandler.GetAlbum(words[2])
         }
     default:
         return "", errors.New("invalid request")
@@ -73,15 +59,9 @@ func processPostRequest(words []string, db *sql.DB) (string, error) {
     case "songs":
         songsHandler := handlers.NewSongHandler(repositories.NewSongRepository(db))
         return songsHandler.CreateSong(words[2], words[3], words[4], words[5], words[6])
-    case "users":
-        usersHandler := handlers.NewUserHandler(repositories.NewUserRepository(db))
-        return usersHandler.CreateUser(words[2], words[3])
     case "playlists":
         playlistsHandler := handlers.NewPlaylistHandler(repositories.NewPlaylistRepository(db))
         return playlistsHandler.CreatePlaylist(words[2], words[3], words[4], words[5], words[6])
-    case "albums":
-        albumsHandler := handlers.NewAlbumHandler(repositories.NewAlbumRepository(db))
-        return albumsHandler.CreateAlbum(words[2], words[3], words[4], words[5], words[6])
     default:
         return "", errors.New("invalid request")
     }
@@ -93,15 +73,9 @@ func processPutRequest(words []string, db *sql.DB) (string, error) {
     case "songs":
         songsHandler := handlers.NewSongHandler(repositories.NewSongRepository(db))
         return songsHandler.UpdateSong(words[2], words[3], words[4], words[5], words[6], words[7])
-    case "users":
-        usersHandler := handlers.NewUserHandler(repositories.NewUserRepository(db))
-        return usersHandler.UpdateUser(words[2], words[3], words[4])
     case "playlists":
         playlistsHandler := handlers.NewPlaylistHandler(repositories.NewPlaylistRepository(db))
         return playlistsHandler.UpdatePlaylist(words[2], words[3], words[4], words[5], words[6], words[7])
-    case "albums":
-        albumsHandler := handlers.NewAlbumHandler(repositories.NewAlbumRepository(db))
-        return albumsHandler.UpdateAlbum(words[2], words[3], words[4], words[5], words[6], words[7])
     default:
         return "", errors.New("invalid request")
     }
@@ -113,15 +87,9 @@ func processDeleteRequest(words []string, db *sql.DB) (string, error) {
     case "songs":
         songsHandler := handlers.NewSongHandler(repositories.NewSongRepository(db))
         return songsHandler.RemoveSong(words[2])
-    case "users":
-        usersHandler := handlers.NewUserHandler(repositories.NewUserRepository(db))
-        return usersHandler.RemoveUser(words[2])
     case "playlists":
         playlistsHandler := handlers.NewPlaylistHandler(repositories.NewPlaylistRepository(db))
         return playlistsHandler.RemovePlaylist(words[2])
-    case "albums":
-        albumsHandler := handlers.NewAlbumHandler(repositories.NewAlbumRepository(db))
-        return albumsHandler.RemoveAlbum(words[2])
     default:
         return "", errors.New("invalid request")
     }
